27fed3bfc2e18f81ae70b295f1eeeb65c045a2b4
Shipease2 <suneel.verma@shipease.in>
2024-01-01 20:25:55 +0530

    dwuehduw

--- dashboardproject/dashboardproject/settings.py
+++ dashboardproject/dashboardproject/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-######add change setting for git
+######add change setting for git###########1111
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
